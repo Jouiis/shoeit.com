dc7493ba025c5621bfa0c32c49bd1f82cec2727b
j <j@js-MacBook-Air.local>
2022-06-02 17:33:21 +0200

    Shoe it

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+    return (
+        <div>
+            <header>
+                <section className={'header_wrapper'}>
+                    <a href={'/'}>Shoe !t</a>
+                </section>
+            </header>
+            <main>
+                List Products
+            </main>
+        </div>
+    );
+}
+
+export default App;
