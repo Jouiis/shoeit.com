d9a48a9944581bb87b71957a6f2dd904991c9b68
j <j@js-MacBook-Air.local>
2022-06-02 18:29:09 +0200

    Shoe it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,39 @@
+import data from "./data";
+
 function App() {
-    return (
-        <div>
-            <header>
-                <section className={'header_wrapper'}>
-                    <a href={'/'}>Shoe !t</a>
-                </section>
-            </header>
-            <main>
-                List Products
-            </main>
-        </div>
-    );
+    return (<div>
+        <header>
+            <section className={'section-header'}>
+                <div className={'header-wrapper'}>
+                    <div className={'container'}>
+                        <a href={'/'}>Shoe !t</a>
+                    </div>
+                </div>
+            </section>
+        </header>
+
+        <main>
+            <section className={'section-products'}>
+                <div className={'container'}>
+                    <h1>Featured Products</h1>
+                    <div className={'products-wrapper'}>
+                        {data.products.map(product => (<div className={'products-wrapper__item'} key={product.slug}>
+                            <a href={`/product/${product.slug}`}>
+                                <img src={product.image} alt={product.name}/>
+                            </a>
+                            <div className={'products-wrapper__item__info'}>
+                                <a href={`/product/${product.slug}`}>
+                                    <p>{product.name}</p>
+                                </a>
+                                <p>{product.price} €</p>
+                                <button>Add to cart</button>
+                            </div>
+                        </div>))}
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>);
 }
 
 export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,38 @@
+const data = {
+    products: [{
+        name: 'Nike Air Force 1 \'07',
+        slug: 'nike-air-force-1-07',
+        category: 'Lifestyle',
+        image: '/images/nikeaf1.webp',
+        price: 120,
+        countInStock: 20,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'The radiance lives on in the Nike Air Force 1 \'07, the b-ball OG that puts a fresh spin on what you know best: durably stitched overlays, clean finishes and the perfect amount of flash to make you shine.'
+    }, {
+        name: 'Nike Air VaporMax Plus',
+        slug: 'nike-air-vapormax-plus',
+        category: 'Running',
+        image: '/images/nikeavpp.webp',
+        price: 210,
+        countInStock: 20,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'The Nike Air VaporMax Plus looks to the past and propels you into the future. Nodding to the 1998 Air Max Plus with its floating cage, padded upper and heel logo, it adds revolutionary VaporMax Air technology to ramp up the comfort and create a modern look.'
+    }, {
+        name: 'Nike Blazer Low Jumbo',
+        slug: 'nike-blazer-low-jumbo',
+        category: 'Lifestyle',
+        image: '/images/nikeblzlj.webp',
+        price: 100,
+        countInStock: 20,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'They say, "Don\'t fix what works". We say, "Perfect it". A jumbo-sized Swoosh design updates this hoops original for style that\'s hard to ignore. A low-profile silhouette and durable design harness the old-school look you love, while visible foam on the tongue and plush collar details add tried-and-tested comfort—it\'s a no-brainer.'
+    }]
+};
+
+export default data;
